b37bf0ba5328e04192ca072b8e473c25a4fa3b48
Andreas Kloeckner <inform@tiker.net>
2007-07-10 01:14:42 -0400

    Add license headers to each source file.

--- src/python/timestep.py
+++ src/python/timestep.py
@@ -1,3 +1,22 @@
+# Hedge - the Hybrid'n'Easy DG Environment
+# Copyright (C) 2007 Andreas Kloeckner
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+
 _RK4A = [0.0,
         -567301805773.0/1357537059087.0,
         -2404267990393.0/2016746695238.0,
