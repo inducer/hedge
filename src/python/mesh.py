a6f8c3fd364f67d6a11f2552c9f6d5347faa4005
Andreas Kloeckner <inform@tiker.net>
2008-01-29 17:28:37 -0500

    Allow periodicity in unstructured rect meshes.

--- src/python/mesh.py
+++ src/python/mesh.py
@@ -694,13 +694,20 @@ def make_regular_square_mesh(a=-0.5, b=0.5, n=5, periodicity=None,
 
 
 def make_rect_mesh(a=(0,0), b=(1,1), max_area=4e-3, 
-        boundary_tagger=(lambda fvi, el, fn: [])):
+        boundary_tagger=(lambda fvi, el, fn: []),
+        periodicity=None, subdivisions=None):
     """Create a semi-structured rectangular mesh.
 
     @arg a: the lower left hand point of the rectangle
     @arg b: the upper right hand point of the rectangle
     @arg max_area: maximum area of each triangle
+    @arg periodicity: either None, or a tuple of bools specifying whether
+      the mesh is to be periodic in x and y.
+    @arg subdivisions: If not C{None}, this is a 2-tuple specifying
+      the number of facet subdivisions in X and Y.
     """
+    import meshpy.triangle as triangle
+
     def round_trip_connect(start, end):
         for i in range(start, end):
             yield i, i+1
@@ -709,23 +716,65 @@ def make_rect_mesh(a=(0,0), b=(1,1), max_area=4e-3,
     def needs_refinement(vert_origin, vert_destination, vert_apex, area):
         return area > max_area
 
-    points = [a, (b[0],a[1]), b, (a[0],b[1])]
-            
-    import meshpy.triangle as triangle
+    marker2tag = {
+            1: "minus_x", 
+            2: "minus_y", 
+            3: "plus_x", 
+            4: "plus_y", 
+            }
 
+    points = [a, (b[0],a[1]), b, (a[0],b[1])]
+    facets = list(round_trip_connect(0, 3))
+    facet_markers = [2,3,4,1]
+
+    if subdivisions is not None:
+        points, facets, facet_markers = triangle.subdivide_facets(
+                [subdivisions[0], subdivisions[1], 
+                    subdivisions[0], subdivisions[1]],
+                points, facets, facet_markers)
+            
     mesh_info = triangle.MeshInfo()
     mesh_info.set_points(points)
-    mesh_info.set_facets(
-            list(round_trip_connect(0, 3)),
-            4*[1]
-            )
+    mesh_info.set_facets(facets, facet_markers)
+    
+    if periodicity is None:
+        periodicity = (False, False)
+
+    axes = ["x", "y"]
+    mesh_periodicity = []
+    periodic_tags = set()
+    for i, axis in enumerate(axes):
+        if periodicity[i]:
+            minus_tag = "minus_"+axis
+            plus_tag = "plus_"+axis
+            mesh_periodicity.append((minus_tag, plus_tag))
+            periodic_tags.add(minus_tag)
+            periodic_tags.add(plus_tag)
+        else:
+            mesh_periodicity.append(None)
 
     generated_mesh = triangle.build(mesh_info, 
-            refinement_func=needs_refinement)
+            refinement_func=needs_refinement,
+            allow_boundary_steiner=not (periodicity[0] or periodicity[1]))
+
+    fvi2fm = dict((frozenset(fvi), marker) for fvi, marker in
+        zip(generated_mesh.facets, generated_mesh.facet_markers))
+
+    def wrapped_boundary_tagger(fvi, el, fn):
+        btag = marker2tag[fvi2fm[frozenset(fvi)]]
+        if btag in periodic_tags:
+            print btag, fvi2fm[frozenset(fvi)], el.face_normals[fn]
+            lfvi = list(fvi)
+            print points[lfvi[0]], points[lfvi[1]]
+            return [btag]
+        else:
+            return [btag] + boundary_tagger(fvi, el, fn)
+
     return make_conformal_mesh(
             generated_mesh.points,
             generated_mesh.elements,
-            boundary_tagger)
+            wrapped_boundary_tagger,
+            periodicity=mesh_periodicity)
 
 
 
