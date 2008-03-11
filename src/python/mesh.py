bdcaf22d4e9f8f1a7924eee48547bfbaf5d7cef9
Andreas Kloeckner <inform@tiker.net>
2008-03-11 13:08:27 -0400

    Add finish_2d_rect_mesh(). Adapt to changed meshpy refinement func arguments.

--- src/python/mesh.py
+++ src/python/mesh.py
@@ -774,54 +774,16 @@ def make_regular_square_mesh(a=-0.5, b=0.5, n=5, periodicity=None,
 
 
 
-def make_rect_mesh(a=(0,0), b=(1,1), max_area=None, 
-        boundary_tagger=(lambda fvi, el, fn: []),
-        periodicity=None, subdivisions=None,
-        refine_func=None):
-    """Create an unstructured rectangular mesh.
-
-    @arg a: the lower left hand point of the rectangle
-    @arg b: the upper right hand point of the rectangle
-    @arg max_area: maximum area of each triangle.
-    @arg periodicity: either None, or a tuple of bools specifying whether
-      the mesh is to be periodic in x and y.
-    @arg subdivisions: If not C{None}, this is a 2-tuple specifying
-      the number of facet subdivisions in X and Y.
-    @arg refine_func: A refinement function as taken by C{meshpy.triangle.build}.
-    """
+def finish_2d_rect_mesh(points, facets, facet_markers, marker2tag, refine_func,
+        periodicity, boundary_tagger):
+    """Semi-internal bottom-half routine for generation of rectangular 2D meshes."""
     import meshpy.triangle as triangle
 
-    def round_trip_connect(start, end):
-        for i in range(start, end):
-            yield i, i+1
-        yield end, start
-
-    if max_area is not None:
-        if refine_func is not None:
-            raise ValueError, "cannot specify both refine_func and max_area"
-        def refine_func(vert_origin, vert_destination, vert_apex, area):
-            return area > max_area
-
-    marker2tag = {
-            1: "minus_x", 
-            2: "minus_y", 
-            3: "plus_x", 
-            4: "plus_y", 
-            }
-
-    points = [a, (b[0],a[1]), b, (a[0],b[1])]
-    facets = list(round_trip_connect(0, 3))
-    facet_markers = [2,3,4,1]
-
-    if subdivisions is not None:
-        points, facets, facet_markers = triangle.subdivide_facets(
-                [subdivisions[0], subdivisions[1], 
-                    subdivisions[0], subdivisions[1]],
-                points, facets, facet_markers)
-            
     mesh_info = triangle.MeshInfo()
     mesh_info.set_points(points)
     mesh_info.set_facets(facets, facet_markers)
+
+    #triangle.write_gnuplot_mesh("mesh.dat", mesh_info, True)
     
     if periodicity is None:
         periodicity = (False, False)
@@ -862,6 +824,58 @@ def make_rect_mesh(a=(0,0), b=(1,1), max_area=None,
 
 
 
+def make_rect_mesh(a=(0,0), b=(1,1), max_area=None, 
+        boundary_tagger=(lambda fvi, el, fn: []),
+        periodicity=None, subdivisions=None,
+        refine_func=None):
+    """Create an unstructured rectangular mesh.
+
+    @arg a: the lower left hand point of the rectangle
+    @arg b: the upper right hand point of the rectangle
+    @arg max_area: maximum area of each triangle.
+    @arg periodicity: either None, or a tuple of bools specifying whether
+      the mesh is to be periodic in x and y.
+    @arg subdivisions: If not C{None}, this is a 2-tuple specifying
+      the number of facet subdivisions in X and Y.
+    @arg refine_func: A refinement function as taken by C{meshpy.triangle.build}.
+    """
+    import meshpy.triangle as triangle
+
+    def round_trip_connect(start, end):
+        for i in range(start, end):
+            yield i, i+1
+        yield end, start
+
+    if max_area is not None:
+        if refine_func is not None:
+            raise ValueError, "cannot specify both refine_func and max_area"
+        def refine_func(vertices, area):
+            return area > max_area
+
+    marker2tag = {
+            1: "minus_x", 
+            2: "minus_y", 
+            3: "plus_x", 
+            4: "plus_y", 
+            }
+
+    points = [a, (b[0],a[1]), b, (a[0],b[1])]
+    facets = list(round_trip_connect(0, 3))
+    facet_markers = [2,3,4,1]
+
+    if subdivisions is not None:
+        points, facets, facet_markers = triangle.subdivide_facets(
+                [subdivisions[0], subdivisions[1], 
+                    subdivisions[0], subdivisions[1]],
+                points, facets, facet_markers)
+            
+    from hedge.mesh import finish_2d_rect_mesh
+    return finish_2d_rect_mesh(points, facets, facet_markers, marker2tag, 
+            refine_func, periodicity, boundary_tagger)
+
+
+
+
 
 def make_square_mesh(a=-0.5, b=0.5, max_area=4e-3, 
         boundary_tagger=(lambda fvi, el, fn: [])):
@@ -885,7 +899,7 @@ def make_disk_mesh(r=0.5, faces=50, max_area=4e-3,
             yield i, i+1
         yield end, start
 
-    def needs_refinement(vert_origin, vert_destination, vert_apex, area):
+    def needs_refinement(vertices, area):
         return area > max_area
 
     points = [(r*cos(angle), r*sin(angle))
