c417a9c952636bbce80ba03489f453efd79c5e79
Andreas Kloeckner <inform@tiker.net>
2009-06-14 16:20:33 -0400

    Make 'gen-bessel-zeros' importable without side effects (for py.test).

--- examples/maxwell/generate-bessel-zeros.py
+++ examples/maxwell/generate-bessel-zeros.py
@@ -1,10 +1,14 @@
-import scipy.special
+def main():
+    import scipy.special
 
-maxnu = 10
-n_zeros = 20
+    maxnu = 10
+    n_zeros = 20
 
-zeros = []
-for n in range(0,maxnu+1):
-    zeros.append(list(scipy.special.jn_zeros(n, n_zeros)))
+    zeros = []
+    for n in range(0,maxnu+1):
+        zeros.append(list(scipy.special.jn_zeros(n, n_zeros)))
 
-outf = open("bessel_zeros.py", "w").write("bessel_zeros = %s" % zeros)
+    outf = open("bessel_zeros.py", "w").write("bessel_zeros = %s" % zeros)
+
+if __name__ == "__main__:
+    main()
