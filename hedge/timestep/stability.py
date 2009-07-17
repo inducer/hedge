d188503333e88c62311a1cbf4638417c71d1b9f5
Andreas Stock <astock@dam.brown.edu>
2009-07-16 23:28:18 -0400

    Update of stability region calculator to new timestepper setup.

--- hedge/timestep/stability.py
+++ hedge/timestep/stability.py
@@ -18,6 +18,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see U{http://www.gnu.org/licenses/}.
 """
+import numpy
+from pytools import memoize
 
 
 
