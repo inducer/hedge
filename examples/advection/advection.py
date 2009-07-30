c1c8400d55965b49fc5949d6582ce75508213468
balzani57 <balzani57@balzani57-laptop.(none)>
2009-07-30 03:07:25 -0400

    test to see if comment in advectin.py shows up in master

--- examples/advection/advection.py
+++ examples/advection/advection.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
+#will this change go to scott's working branch?
 
 
 from __future__ import division
