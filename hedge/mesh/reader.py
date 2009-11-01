c410dfac8017442b26b3bd9e200adf101328e6e6
Xueyu Zhu <xzhu@tuck.ath.cx>
2009-11-01 15:57:46 -0500

    add class Gmshcontainer to store information from gmsh file

--- hedge/mesh/reader.py
+++ hedge/mesh/reader.py
@@ -26,58 +26,72 @@ from pytools import memoize
 # gmsh nodes-per-elements
 elm_type =(2,3,4,4,8,6,5,3,6,9,10,27,18,14,1,8,20,15,13,9,10,12,15,15,21,12,15,15,18,20,35,56)
 
-class Mesh:
-
+class GmshContainer:
+    """
+    Data structure to store the information from gmsh file
+    """
     def __init__(self):
         self.Nodes    = []
         self.Elements = []
         self.phy_tags = []
-    """    self.element_type_map = self._get_gmsh_element_type_to_info_map()
-     
+        self.element_type_map = self._get_gmsh_element_type_to_info_map()
+
     @memoize
     def _get_gmsh_element_type_to_info_map(self):
         from pytools import Record
-        class ElementTypeInfo(Record): pass
-        
+        class ElementTypeInfo(Record):
+            __slots__ = ['ele_type']
         from hedge.element import TriangularElement,TetrahedralElement
-        print ElementTypeInfo(TriangularElement(1))
         return {
-                1:  "Line",
-                2:  ElementTypeInfo(TriangularElement(1)),
+                1:  "2-nodes Line",
+                2:  ElementTypeInfo(ele_type = TriangularElement(1)),
+                3:  "4-nodes quadrangle",
+                4:  ElementTypeInfo(ele_type = TetrahedralElement(1)),
+                5:  "8-nodes hexahedron",
+                6:  "6-nodes prism",
+                7:  "5-nodes pyramid",
                 8:  "2nd order Line",
-                9:  ElementTypeInfo(TriangularElement(2)),
-                11: ElementTypeInfo(TetrahedralElement(2)),
-                20: ElementTypeInfo(TetrahedralElement(3)),
-                21: ElementTypeInfo(TetrahedralElement(3)),
-                22: ElementTypeInfo(TetrahedralElement(4)),
-                23: ElementTypeInfo(TetrahedralElement(4)),
-                24: ElementTypeInfo(TetrahedralElement(5)),
-                25: ElementTypeInfo(TetrahedralElement(5)),
-                26: ElementTypeInfo(TetrahedralElement(4)),
-                27: ElementTypeInfo(TetrahedralElement(4)), 
-                28: ElementTypeInfo(TetrahedralElement(4)), 
-                29: ElementTypeInfo(TetrahedralElement(3)),
-                30: ElementTypeInfo(TetrahedralElement(4)),
-                31: ElementTypeInfo(TetrahedralElement(5)),
+                9:  ElementTypeInfo(ele_type = TriangularElement(2)),
+                10: "9-node second order quadrangle",
+                11: ElementTypeInfo(ele_type = TetrahedralElement(2)),
+                12: "27-node second order hexahedron",
+                13: "18-node second order prism",
+                14: "14-node second order pyramid",
+                15: "1-node point",
+                16: "8-node second order quadrangle",
+                17: "20-node second order hexahedron",
+                18: "15-node second order prism",
+                19: "13-node second order pyramid",
+                20: ElementTypeInfo(ele_type = TetrahedralElement(3)),
+                21: ElementTypeInfo(ele_type = TetrahedralElement(3)),
+                22: ElementTypeInfo(ele_type = TetrahedralElement(4)),
+                23: ElementTypeInfo(ele_type = TetrahedralElement(4)),
+                24: ElementTypeInfo(ele_type = TetrahedralElement(5)),
+                25: ElementTypeInfo(ele_type = TetrahedralElement(5)),
+                26: ElementTypeInfo(ele_type = TetrahedralElement(4)),
+                27: ElementTypeInfo(ele_type = TetrahedralElement(4)), 
+                28: ElementTypeInfo(ele_type = TetrahedralElement(4)), 
+                29: ElementTypeInfo(ele_type = TetrahedralElement(3)),
+                30: ElementTypeInfo(ele_type = TetrahedralElement(4)),
+                31: ElementTypeInfo(ele_type = TetrahedralElement(5)),
                 }
-   """
+   
 
 
 def read_gmsh(filename):
     """
-    mesh reader for gmsh
+    mesh reader for gmsh file
     """
 
     # initialize Mesh class
-    readed_mesh = Mesh()
-
+    readed_mesh = GmshContainer()
+    
     # open target file
     lines = open(filename, 'r').readlines()
     i = 0
     while i < len(lines):
         l = lines[i].strip()
-        i += 1 
-        
+        i += 1  
         if l == "$MeshFormat" :
             while True:
                 i+=1
@@ -112,7 +126,7 @@ def read_gmsh(filename):
                 readed_mesh.Elements.append(
                                             dict(
                                                 ele_indices = lvalue[0],
-                                                el_type     = lvalue[1],
+                                                el_type     = readed_mesh.element_type_map[type],
                                                 ele_number_of_tags =lvalue[2],
                                                 el_tags = lvalue[3:3+lvalue[2]],
                                                 nodes   = lvalue[-elm_type[type-1]:]
@@ -132,7 +146,8 @@ def read_gmsh(filename):
                 l_str = l.split()    
                 lvalue = [int(x) for x in l_str[:-1]] 
                 readed_mesh.phy_tags.append(
-                                            dict(phy_dimension = lvalue[0],
+                                            dict(
+                                                 phy_dimension = lvalue[0],
                                                  tag_index = lvalue[1],
                                                  tag_name  = l_str[-1].replace('\"',' ')
                                                  )
@@ -168,8 +183,10 @@ def read_gmsh(filename):
                 i=len(lines)-1
                 if i>=len(lines)-1:
                     break
-    #print readed_mesh.phy_tags
-    return 0
+
+    #close the file explicitly
+    open(filename, 'r').close
+    return readed_mesh
 
 
 
