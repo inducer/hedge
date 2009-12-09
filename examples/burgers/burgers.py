822acd67577b71f16c7c5caf8e112a6430c87e4a
Andreas Kloeckner <inform@tiker.net>
2009-12-09 00:43:12 -0500

    Adapt Burgers example to Persson-Peraire.

--- examples/burgers/burgers.py
+++ examples/burgers/burgers.py
@@ -23,7 +23,6 @@ import numpy.linalg as la
 
 
 def main(write_output=True, flux_type_arg="upwind"):
-    from hedge.timestep import RK4TimeStepper
     from hedge.tools import mem_checkpoint
     from math import sin, cos, pi, sqrt
     from math import floor
@@ -32,36 +31,44 @@ def main(write_output=True, flux_type_arg="upwind"):
     rcon = guess_run_context()
 
     def u0(x, el):
-        return -sin(pi*x[0])
+        return -sin(pi*x[0]+0.03)
 
+    el_count = 11
+    order = 5
     if rcon.is_head_rank:
         from hedge.mesh.generator import make_uniform_1d_mesh
-        mesh = make_uniform_1d_mesh(-1, 1, 100, periodic=True)
+        mesh = make_uniform_1d_mesh(-1, 1, el_count, periodic=True)
 
     if rcon.is_head_rank:
         mesh_data = rcon.distribute_mesh(mesh)
     else:
         mesh_data = rcon.receive_mesh()
 
-    discr = rcon.make_discretization(mesh_data, order=5)
+    discr = rcon.make_discretization(mesh_data, order=order)
+    #vis_discr = rcon.make_discretization(mesh_data, order=30)
     vis_discr = discr
 
-    from hedge.visualization import VtkVisualizer
+    from hedge.discretization import Projector
+    vis_proj = Projector(discr, vis_discr)
+
+    from hedge.visualization import VtkVisualizer, SiloVisualizer
     if write_output:
-        vis = VtkVisualizer(vis_discr, rcon, "fld")
+        vis = SiloVisualizer(vis_discr, rcon)
+        #vis = VtkVisualizer(vis_discr, rcon, "fld")
 
     # operator setup ----------------------------------------------------------
     from hedge.data import \
             ConstantGivenFunction, \
             TimeConstantGivenFunction, \
             TimeDependentGivenFunction
+    from hedge.tools.second_order import \
+            LDGSecondDerivative, \
+            CentralSecondDerivative
     from hedge.models.burgers import BurgersOperator
-    op = BurgersOperator(mesh.dimensions)
+    op = BurgersOperator(mesh.dimensions,
+            viscosity_scheme=LDGSecondDerivative())
 
     u = discr.interpolate_volume_function(u0)
-
-    # timestep setup ----------------------------------------------------------
-    stepper = RK4TimeStepper()
 
     if rcon.is_head_rank:
         print "%d elements" % len(discr.mesh.elements)
@@ -83,8 +90,6 @@ def main(write_output=True, flux_type_arg="upwind"):
     add_simulation_quantities(logmgr)
     discr.add_instrumentation(logmgr)
 
-    stepper.add_instrumentation(logmgr)
-
     from hedge.log import Integral, LpNorm
     u_getter = lambda: u
     logmgr.add_quantity(Integral(u_getter, discr, name="int_u"))
@@ -94,19 +99,42 @@ def main(write_output=True, flux_type_arg="upwind"):
     logmgr.add_watches(["step.max", "t_sim.max", "l2_u", "t_step.max"])
 
     # timestep loop -----------------------------------------------------------
-    rhs = op.bind(discr)
+    h = 2/el_count
+    from hedge.tools import PerssonPeraireDiscontinuitySensor
+    sensor = PerssonPeraireDiscontinuitySensor(discr, kappa=5,
+            eps0=h/order, s_0=1/order**4)
+
+    rhs = op.bind(discr, sensor=sensor)
+
+    from hedge.timestep import RK4TimeStepper
+    from hedge.timestep.dumka3 import Dumka3TimeStepper
+    #stepper = RK4TimeStepper()
+    stepper = Dumka3TimeStepper(3)
+    #stepper = Dumka3TimeStepper(4)
+
+    stepper.add_instrumentation(logmgr)
 
     try:
         from hedge.timestep import times_and_steps
+        # for visc=0.01
+        #stab_fac = 0.1 # RK4
+        stab_fac = 1.6 # dumka3(3), central
+        #stab_fac = 3 # dumka3(4), central
+
+        #stab_fac = 0.01 # RK4
+        stab_fac = 0.5 # dumka3(3), central
+        #stab_fac = 3 # dumka3(4), central
+
+        dt = stab_fac*op.estimate_timestep(discr,
+                stepper=RK4TimeStepper(), t=0, fields=u)
+
         step_it = times_and_steps(
-                final_time=3, logmgr=logmgr,
-                max_dt_getter=lambda t: op.estimate_timestep(discr,
-                    stepper=stepper, t=t, fields=u))
+                final_time=5, logmgr=logmgr, max_dt_getter=lambda t: dt)
 
         for step, t, dt in step_it:
-            if step % 5 == 0 and write_output:
+            if step % 100 == 0 and write_output:
                 visf = vis.make_file("fld-%04d" % step)
-                vis.add_data(visf, [ ("u", u), ],
+                vis.add_data(visf, [ ("u", vis_proj(u)), ],
                             time=t,
                             step=step
                             )
--- hedge/models/burgers.py
+++ hedge/models/burgers.py
@@ -28,16 +28,21 @@ import numpy.linalg as la
 
 from hedge.models import HyperbolicOperator
 import hedge.data
+from hedge.tools.second_order import CentralSecondDerivative
 
 
 
 
 class BurgersOperator(HyperbolicOperator):
-    def __init__(self, dimensions):
+    def __init__(self, dimensions, viscosity=None, 
+            viscosity_scheme=CentralSecondDerivative()):
         # yes, you read that right--no BCs, 1D only.
+        # (well--you can run the operator on a 2D grid. If you must.)
         self.dimensions = dimensions
+        self.viscosity = viscosity
+        self.viscosity_scheme = viscosity_scheme
 
-    def op_template(self):
+    def op_template(self, with_sensor):
         from hedge.optemplate import \
                 Field, \
                 BoundaryPair, \
@@ -45,7 +50,8 @@ class BurgersOperator(HyperbolicOperator):
                 make_minv_stiffness_t, \
                 make_nabla, \
                 InverseMassOperator, \
-                ElementwiseMaxOperator
+                ElementwiseMaxOperator, \
+                ScalarParameter
 
         u = Field("u")
 
@@ -64,20 +70,57 @@ class BurgersOperator(HyperbolicOperator):
                 emax_u, 
                 make_obj_array([u]), 
                 [make_obj_array([flux(u)])], 
-                [], strong=True)
+                [], strong=False)
 
-        return -(nabla[0](flux(u))) + m_inv(lf_flux[0])
+        from hedge.tools.second_order import SecondDerivativeTarget
 
-    def max_eigenvalue(self, t=None, fields=None, discr=None):
-        return discr.nodewise_max(fields)
+        if self.viscosity is not None or with_sensor:
+            # strong_form here allows LDG to reuse the value of grad u.
+            grad_tgt = SecondDerivativeTarget(
+                    self.dimensions, strong_form=True,
+                    operand=u)
 
-    def bind(self, discr):
-        compiled_op_template = discr.compile(self.op_template())
+            self.viscosity_scheme.grad(grad_tgt, bc_getter=None,
+                    dirichlet_tags=[], neumann_tags=[])
+
+            div_tgt = SecondDerivativeTarget(
+                    self.dimensions, strong_form=False,
+                    operand=grad_tgt.minv_all)
+
+            self.viscosity_scheme.div(div_tgt,
+                    bc_getter=None,
+                    dirichlet_tags=[], neumann_tags=[])
+
+            viscosity_coeff = 0
+            if with_sensor:
+                viscosity_coeff += Field("sensor")
+
+            if isinstance(self.viscosity, float):
+                viscosity_coeff += self.viscosity
+            if self.viscosity is None:
+                pass
+            else:
+                raise TypeError("unsupported type of viscosity coefficient")
+
+            viscosity_bit = Field("sensor")*div_tgt.minv_all
+        else:
+            viscosity_bit = 0
+
+        return (minv_st[0](flux(u))) - m_inv(lf_flux[0]) \
+                + viscosity_bit
+
+    def bind(self, discr, sensor=None):
+        compiled_op_template = discr.compile(
+                self.op_template(with_sensor=sensor is not None))
 
         from hedge.mesh import check_bc_coverage
         check_bc_coverage(discr.mesh, [])
 
         def rhs(t, u):
-            return compiled_op_template(u=u)
+            return compiled_op_template(u=u,
+                    sensor=sensor(u))
 
         return rhs
+
+    def max_eigenvalue(self, t=None, fields=None, discr=None):
+        return discr.nodewise_max(fields)
