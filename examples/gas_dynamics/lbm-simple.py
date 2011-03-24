08535cb79e8234c82a1b1988635c6260032eebcd
Andreas Kloeckner <inform@tiker.net>
2011-03-23 22:54:47 -0400

    Add first working LBM implementation.

---
+++ examples/gas_dynamics/lbm-simple.py
@@ -0,0 +1,127 @@
+# Hedge - the Hybrid'n'Easy DG Environment
+# Copyright (C) 2011 Andreas Kloeckner
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+
+from __future__ import division
+import numpy as np
+import numpy.linalg as la
+
+
+
+
+def main(write_output=True, dtype=np.float32):
+    from hedge.backends import guess_run_context
+    rcon = guess_run_context()
+
+    from hedge.mesh.generator import make_rect_mesh
+    if rcon.is_head_rank:
+        mesh = make_rect_mesh(a=(-1,-1),b=(1,1), max_area=0.008/16,
+                periodicity=(True,True),
+                subdivisions=(40, 40))
+
+    from hedge.models.gas_dynamics.lbm import \
+            D2Q9LBMMethod, LatticeBoltzmannOperator
+
+    op = LatticeBoltzmannOperator(
+            D2Q9LBMMethod(), lbm_delta_t=0.01, nu=1)
+
+    if rcon.is_head_rank:
+        print "%d elements" % len(mesh.elements)
+        mesh_data = rcon.distribute_mesh(mesh)
+    else:
+        mesh_data = rcon.receive_mesh()
+
+    discr = rcon.make_discretization(mesh_data, order=4,
+            default_scalar_type=dtype,
+            debug=["cuda_no_plan"])
+    from hedge.timestep.runge_kutta import LSRK4TimeStepper
+    stepper = LSRK4TimeStepper(dtype=dtype)
+
+    from hedge.visualization import VtkVisualizer
+    if write_output:
+        vis = VtkVisualizer(discr, rcon, "fld")
+
+    from hedge.tools import join_fields
+    f_bar = join_fields(
+            [discr.volume_empty(dtype=dtype) 
+                for i in range(len(op.method))])
+    from hedge.data import CompiledExpressionData
+    def ic_expr(t, x, fields):
+        from hedge.optemplate import CFunction
+        from pymbolic.primitives import IfPositive
+        from pytools.obj_array import make_obj_array
+        return make_obj_array([
+            1 + 0.5*IfPositive(x[1],
+                CFunction("sin")(np.pi*x[0]),
+                -CFunction("sin")(np.pi*x[0]))
+            ]*len(op.method))
+
+    f_bar = CompiledExpressionData(ic_expr).volume_interpolant(0, discr)
+
+    # timestep loop -----------------------------------------------------------
+    stream_rhs = op.bind_rhs(discr)
+    collision_update = op.bind(discr, op.collision_update)
+    get_rho = op.bind(discr, op.rho)
+    get_rho_u = op.bind(discr, op.rho_u)
+
+    final_time = 10
+    try:
+        lbm_dt = op.lbm_delta_t
+        dg_dt = op.estimate_timestep(discr, stepper=stepper)
+
+        dg_steps_per_lbm_step = int(np.ceil(lbm_dt / dg_dt))
+        dg_dt = lbm_dt / dg_steps_per_lbm_step
+
+        lbm_steps = int(final_time // op.lbm_delta_t)
+        for step in xrange(lbm_steps):
+            t = step*lbm_dt
+
+            if step % 10 == 0 and write_output:
+                visf = vis.make_file("fld-%04d" % step)
+
+                rho = get_rho(f_bar)
+                rho_u = get_rho_u(f_bar)
+                vis.add_data(visf,
+                        [ ("fbar%d" %i, 
+                            discr.convert_volume(f_bar_i, "numpy")) for i, f_bar_i in enumerate(f_bar)]+
+                        [
+                            ("rho", discr.convert_volume(rho, "numpy")),
+                            ("rho_u", discr.convert_volume(rho_u, "numpy")),
+                        ],
+                        time=t,
+                        step=step)
+                visf.close()
+
+            print "step=%d, t=%f" % (step, t)
+
+            for substep in range(dg_steps_per_lbm_step):
+                f_bar = stepper(f_bar, t + substep*dg_dt, dg_dt, stream_rhs)
+
+            f_bar = collision_update(f_bar)
+
+    finally:
+        if write_output:
+            vis.close()
+
+        discr.close()
+
+
+
+
+if __name__ == "__main__":
+    main(True)
---
+++ hedge/models/gas_dynamics/lbm.py
@@ -0,0 +1,205 @@
+# -*- coding: utf8 -*-
+"""Lattice-Boltzmann operator."""
+
+from __future__ import division
+
+__copyright__ = "Copyright (C) 2011 Andreas Kloeckner"
+
+__license__ = """
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see U{http://www.gnu.org/licenses/}.
+"""
+
+import numpy as np
+import numpy.linalg as la
+from hedge.models import HyperbolicOperator
+from pytools.obj_array import make_obj_array
+
+
+
+
+
+class LBMMethodBase(object):
+    def __len__(self):
+        return len(self.direction_vectors)
+
+    def find_opposites(self):
+        self.opposites = np.zeros(len(self))
+
+        for alpha in xrange(len(self)):
+            if self.opposites[alpha]:
+                continue
+
+            found = False
+            for alpha_2 in xrange(alpha, len(self)):
+                if la.norm(
+                        self.direction_vectors[alpha] 
+                        + self.direction_vectors[alpha_2]) < 1e-12:
+                    self.opposites[alpha] = alpha_2
+                    self.opposites[alpha_2] = alpha
+                    found = True
+
+            if not found:
+                raise RuntimeError(
+                        "direction %s had no opposite" 
+                        % self.direction_vectors[alpha])
+
+
+
+
+class D2Q9LBMMethod(LBMMethodBase):
+    def __init__(self):
+        self.dimensions = 2
+
+        alphas = np.arange(0, 9)
+        thetas = (alphas-1)*np.pi/2
+        thetas[5:9] += np.pi/4
+
+        direction_vectors = np.vstack([
+            np.cos(thetas), np.sin(thetas)]).T
+
+        direction_vectors[0] *= 0
+        direction_vectors[5:9] *= np.sqrt(2)
+
+        direction_vectors[np.abs(direction_vectors) < 1e-12] = 0
+
+        self.direction_vectors = direction_vectors
+
+        self.weights = np.array([4/9] + [1/9]*4 + [1/36]*4)
+
+        self.speed_of_sound = 1/np.sqrt(3)
+        self.find_opposites()
+
+    def f_equilibrium(self, rho, alpha, u):
+        e_alpha = self.direction_vectors[alpha]
+        c_s = self.speed_of_sound
+        return self.weights[alpha]*rho*(
+                1
+                + np.dot(e_alpha, u)/c_s**2
+                + 1/2*np.dot(e_alpha, u)**2/c_s**4
+                - 1/2*np.dot(u, u)**2/c_s**2)
+
+
+
+
+class LatticeBoltzmannOperator(HyperbolicOperator):
+    def __init__(self, method, lbm_delta_t, nu, flux_type="upwind"):
+        self.method = method
+        self.lbm_delta_t = lbm_delta_t
+        self.nu = nu
+
+        self.flux_type = flux_type
+
+    @property
+    def tau(self):
+        return (self.nu
+                /
+                (self.lbm_delta_t*self.method.speed_of_sound**2))
+
+    def get_advection_flux(self, q, velocity):
+        from hedge.flux import make_normal, FluxScalarPlaceholder
+        from pymbolic.primitives import IfPositive
+
+        u = FluxScalarPlaceholder(0)
+        normal = make_normal(self.method.dimensions)
+
+        if self.flux_type == "central":
+            return u.avg*np.dot(normal, velocity)
+        elif self.flux_type == "lf":
+            return u.avg*np.dot(normal, velocity) \
+                    + 0.5*la.norm(v)*(u.int - u.ext)
+        elif self.flux_type == "upwind":
+            return (np.dot(normal, velocity)*
+                    IfPositive(np.dot(normal, velocity),
+                        u.int, # outflow
+                        u.ext, # inflow
+                        ))
+        else:
+            raise ValueError, "invalid flux type"
+
+    def get_advection_op(self, q, velocity):
+        from hedge.optemplate import (
+                BoundaryPair,
+                get_flux_operator,
+                make_stiffness_t,
+                InverseMassOperator)
+
+        stiff_t = make_stiffness_t(self.method.dimensions)
+
+        flux_op = get_flux_operator(self.get_advection_flux(q, velocity))
+        return InverseMassOperator()(
+                np.dot(velocity, stiff_t*q) - flux_op(q))
+
+    def f_bar(self):
+        from hedge.optemplate import make_vector_field
+        return make_vector_field("f_bar", len(self.method))
+
+    def rho(self, f_bar):
+        return sum(f_bar)
+
+    def rho_u(self, f_bar):
+        return sum(
+                dv_i * field_i
+                for dv_i, field_i in
+                zip(self.method.direction_vectors, f_bar))
+
+    def stream_rhs(self, f_bar):
+        return make_obj_array([
+            self.get_advection_op(f_bar_alpha, e_alpha)
+            for e_alpha, f_bar_alpha in
+            zip(self.method.direction_vectors, f_bar)])
+
+    def collision_update(self, f_bar):
+        from hedge.tools.symbolic import make_common_subexpression as cse
+        rho = cse(self.rho(f_bar), "rho")
+        rho_u = self.rho_u(f_bar)
+        u = cse(rho_u/rho, "u")
+
+        f_eq_func = self.method.f_equilibrium
+        f_eq = make_obj_array([
+            f_eq_func(rho, alpha, u) for alpha in range(len(self.method))])
+
+        return f_bar - 1/(self.tau+1/2)*(f_bar - f_eq)
+
+    def bind_rhs(self, discr):
+        compiled_op_template = discr.compile(
+                self.stream_rhs(self.f_bar()))
+
+        #from hedge.mesh import check_bc_coverage, TAG_ALL
+        #check_bc_coverage(discr.mesh, [TAG_ALL])
+
+        def rhs(t, f_bar):
+            return compiled_op_template(f_bar=f_bar)
+
+        return rhs
+
+    def bind(self, discr, what):
+        f_bar_sym = self.f_bar()
+
+        from hedge.optemplate.mappers.type_inference import (
+                type_info, NodalRepresentation)
+
+        type_hints = dict(
+                (f_bar_i, type_info.VolumeVector(NodalRepresentation()))
+                for f_bar_i in f_bar_sym)
+
+        compiled_op_template = discr.compile(what(f_bar_sym), type_hints=type_hints)
+
+        def rhs(f_bar):
+            return compiled_op_template(f_bar=f_bar)
+
+        return rhs
+
+    def max_eigenvalue(self, t=None, fields=None, discr=None):
+        return max(
+                la.norm(v) for v in self.method.direction_vectors)
