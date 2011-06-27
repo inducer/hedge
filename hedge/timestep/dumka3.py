92b3529f6d1411eec9c08164b9b1e64c304adc01
Andreas Kloeckner <inform@tiker.net>
2011-06-26 22:52:51 -0400

    Restore GPLv3 on dumka3 time stepper.

--- hedge/timestep/dumka3.py
+++ hedge/timestep/dumka3.py
@@ -7,23 +7,29 @@ from __future__ import division
 __copyright__ = "Copyright (C) 2009 Alexei Medovikov, Andreas Kloeckner"
 
 __license__ = """
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see U{http://www.gnu.org/licenses/}.
+
+DUMKA3 by Alexei Medovikov,
+based on C++ code retrieved 11/9/2009 from http://www.math.tulane.edu/~amedovik/
+ported by Andreas Kloeckner <kloeckner@dam.brown.edu>
+released under GPL3 by permission from Dr. Medovikov 11/13/2009.
+
+Note: If you use this code for published results, Dr. Medovikov expects (but
+does not require) a citation of his paper
+
+Alexei A. Medovikov, "High order explicit methods for parabolic equations"
+BIT1998, Vol. 38, No.2, pp.372-390
 """
 
 
