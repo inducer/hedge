55be81a937c51b90f91e1847d45f53a6057fc750
Andreas Kloeckner <inform@tiker.net>
2013-06-09 18:57:06 -0400

    Remove reference to pymbolic.simplify

--- test/test_jit_discretization.py
+++ test/test_jit_discretization.py
@@ -958,7 +958,7 @@ def test_elliptic():
     v_x = pymbolic.var("x")
     truesol = pymbolic.parse("math.sin(x[0]**2*x[1]**2)")
     truesol_c = pymbolic.compile(truesol, variables=["x"])
-    rhs = pymbolic.simplify(pymbolic.laplace(truesol, [v_x[0], v_x[1]]))
+    rhs = pymbolic.laplace(truesol, [v_x[0], v_x[1]])
     rhs_c = pymbolic.compile(rhs, variables=["x", "el"])
 
     from hedge.mesh import TAG_ALL, TAG_NONE
